a23260632e95af34bcf65888298477fbf75d78fb
Death-Boy-Dmi <smertindmi@mail.ru>
2016-12-13 22:09:32 +0300

    Заготовка для класса Процессора.

---
+++ CImCPU.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CQueue.h"
+
+#define T 1000
+
+class ImCPU
+{
+	unsigned int tact;
+	CQeue<int> tasks;
+	unsigned int numCopmletedTask;
+	unsigned int numTaskQeue;
+	unsigned int numFailure;
+	unsigned int downtimeTacts;
+public:
+	void Statistics();
+
+	
+};
