1b4d8ab894e9a8aee8805e9d58d9b018ff106f72
Death-Boy-Dmi <smertindmi@mail.ru>
2016-12-24 10:00:56 +0300

    Исправил название теста.

--- Tests/CImCPU_test.cpp
+++ Tests/CImCPU_test.cpp
@@ -11,7 +11,7 @@ TEST(CImCPU, can_create_ImCPU_2)
 	ASSERT_NO_THROW(CImCPU CPU(10, 10, 0.5, 0.5));
 }
 
-TEST(CImCPU, can_create_ImCPU_with_incorrect_values)
+TEST(CImCPU, throw_when_create_ImCPU_with_incorrect_values)
 {
 	ASSERT_ANY_THROW(CImCPU CPU(-10, 10001, 1, -0.5));
 }
